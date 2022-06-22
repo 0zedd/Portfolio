aa1b12639d6dac14c34eff26bb1fd14b92cec31a
Zedd <zeddsnow@gmail.com>
2022-06-22 00:42:56 +0000

    add menu to disapear after click

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const menuim = document.querySelector('.menuim');
 const navexpand = document.querySelector('.nav_expand');
 const iconx = document.querySelector('.iconx');
+const abt = document.querySelector('#abt');
+const con = document.querySelector('#con');
+const por = document.querySelector('#por');
 
 menuim.addEventListener('click', () => {
   menuim.classList.toggle('active');
@@ -8,5 +11,17 @@ menuim.addEventListener('click', () => {
 });
 
 iconx.addEventListener('click', () => {
+  navexpand.classList.toggle('active');
+});
+
+abt.addEventListener('click', () => {
+  navexpand.classList.toggle('active');
+});
+
+con.addEventListener('click', () => {
+  navexpand.classList.toggle('active');
+});
+
+por.addEventListener('click', () => {
   navexpand.classList.toggle('active');
 });
