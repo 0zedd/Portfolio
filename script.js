4083b612d6586874bf217309295918c763f1ab8a
Zedd <zeddsnow@gmail.com>
2022-06-23 15:28:32 +0000

    add syle to desktop popup

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 const menuim = document.querySelector('.menuim');
 const navexpand = document.querySelector('.nav_expand');
 const iconx = document.querySelector('.iconx');
@@ -24,4 +25,30 @@ con.addEventListener('click', () => {
 
 por.addEventListener('click', () => {
   navexpand.classList.toggle('active');
+});
+
+const button0 = document.querySelector('.button0');
+const jsdetails = document.querySelector('.jsdetails');
+const xman = document.querySelector('.xman');
+
+button0.addEventListener('click', () => {
+  button0.classList.toggle('active');
+  jsdetails.classList.toggle('active');
+});
+
+xman.addEventListener('click', () => {
+  jsdetails.classList.toggle('active');
+});
+
+const deskbtn = document.querySelector('#deskbtn');
+const detaildsk = document.querySelector('.detaildsk');
+const desk = document.querySelector('.desk');
+
+deskbtn.addEventListener('click', () => {
+  deskbtn.classList.toggle('active');
+  detaildsk.classList.toggle('active');
+});
+
+desk.addEventListener('click', () => {
+  detaildsk.classList.toggle('active');
 });
